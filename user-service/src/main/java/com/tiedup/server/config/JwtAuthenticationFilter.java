ed5ebb37d577d2d780d84f6aee2a882e311f94c0
akahramn <a.kahramnn@gmail.com>
2025-03-01 02:22:08 +0300

    adding tables

--- user-service/src/main/java/com/tiedup/server/config/JwtAuthenticationFilter.java
+++
@@ -1,6 +0,0 @@
-package com.tiedup.server.config;
-
-import org.springframework.web.filter.OncePerRequestFilter;
-//check every request authentication
-public class JwtAuthenticationFilter extends OncePerRequestFilter {
-}
