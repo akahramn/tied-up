b65d5754fff33da9e0b41558d8b6f8b2fff75df6
akahramn <a.kahramnn@gmail.com>
2025-03-15 00:44:51 +0300

    adding course operations

---
+++ course-service/src/main/java/com/tiedup/course_service/security/SecurityConfig.java
@@ -0,0 +1,41 @@
+package com.tiedup.course_service.security;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+@Configuration
+@EnableWebSecurity
+@RequiredArgsConstructor
+@EnableMethodSecurity
+public class SecurityConfig {
+
+    private final JwtAuthFilter jwtAuthFilter;
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/courses").hasRole("INSTRUCTOR") // Sadece INSTRUCTOR ders oluşturabilir
+                        .requestMatchers("api/v1/course/enroll").hasRole("STUDENT") // Sadece INSTRUCTOR ders oluşturabilir
+                        .requestMatchers("/courses/**").authenticated() // Diğer endpoint'ler JWT doğrulaması gerektirir
+                        .anyRequest().permitAll()
+                )
+                .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class);
+        return http.build();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+}
