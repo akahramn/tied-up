bf221faa69741e527bdbeeb6d5bf50b8a51dfed5
akahramn <a.kahramnn@gmail.com>
2025-03-21 00:24:07 +0300

    kafka adding

--- payment-service/src/main/java/com/tiedup/payment/model/Transaction.java
+++ payment-service/src/main/java/com/tiedup/payment/model/Transaction.java
@@ -8,7 +8,7 @@ import java.time.LocalDateTime;
 import java.util.UUID;
 
 @Entity
-@Table(name = "transactions")
+@Table(name = "transaction", schema = "payment")
 @Getter
 @Setter
 @NoArgsConstructor
